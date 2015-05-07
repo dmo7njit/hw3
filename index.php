fa5af282c3ab81cf8aa56711a86e199c625e10e6
David O'Grady <dmo7@njit.edu>
2015-05-06 23:47:34 -0400

    updated the working router page

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <html>
-<head>
-<title>Homework 2</title>
-</head>
+	<head>
+		<title>MySQL Queries</title>
+	</head>
 <body>
 
 <?php
-	echo "Homework 2";
+
+	error_reporting(-1);
+	ini_set('display_errors','On');
+	include 'pages.php';
+
+	$db = new PDO('mysql:host=localhost;dbname=employees;charset=utf8','root','jared1harry',array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+	
+
+	if(isset($_REQUEST['page'])){
+		$qry = new $_REQUEST['page'];
+	} else {
+
+	echo "<table>";
+	echo '<tr><th><a href="?page=page1"> Who is the highest paid employee?</a></th></tr>';
+	echo '<tr><th><a href="?page=page2"> Who is the highest paid employee between 1985 and 1981?</a></th></tr>';
+	echo '<tr><th><a href="?page=page3"> Which department currently has highest paid department manager?</a></th></tr>';
+	echo '<tr><th><a href="?page=page4"> What are the titles of all the departments?</a></th></tr>';
+	echo '<tr><th><a href="?page=page5"> Who are the current department heads?</a></th></tr>';
+	echo '<tr><th><a href="?page=page6"> Who is highest paid employee that is not a department head?</a></th></tr>';
+	echo '<tr><th><a href="?page=page7"> Who is currently the lowest paid employee?</a></th></tr>';
+	echo '<tr><th><a href="?page=page8"> How many employees currently work in each department?</a></th></tr>';
+	echo '<tr><th><a href="?page=page9"> How much does each department currently spend on salaries?</a></th></tr>';
+	echo '<tr><th><a href="?page=page10"> How much is currently spent for all salaries?</a></th></tr>';
+	echo '<tr><th><a href="?page=pageinsert">Insert entry.</a></th></tr>';
+	echo '<tr><th><a href="?page=pageupdate">Update entry.</a></th></tr>';
+	echo "</table>";
+
+	}
+
+
 ?>
+
 </body>
 </html>
-
